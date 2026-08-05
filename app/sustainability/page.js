243289df157461b23d2f4b1f8e06cd5e9e542681
cybrosoft <salman@cybrosoft.com>
2026-08-05 04:44:09 +0300

    header 1

--- app/sustainability/page.js
+++ app/sustainability/page.js
@@ -1,7 +1,6 @@
 "use client";
 
 import { motion } from "framer-motion";
-import HeaderTranspirant from "@/components/Header/HeaderTranspirant";
 import Footer from "@/components/Footer/index";
 import BreadcrumbSection from "@/components/Breadcrumbs";
 import HeroMedia from "@/components/HeroMedia";
@@ -13,12 +12,13 @@ import CertificationsSection from "./CertificationsSection";
 import ContinueReading from "@/components/ContinueReading";
 import WhistleblowerCtaSection from "@/components/WhistleblowerCtaSection";
 import { sustainabilityPage } from "./data";
+import HeaderWhite from "@/components/Header/HeaderWhite";
 
 
 export default function AboutPage() {
   return (
     <main className="min-h-screen bg-white text-neutral-900">
-      <HeaderTranspirant />
+      <HeaderWhite />
 
       {/* ✅ New editorial hero typography */}
       <HeroMedia
